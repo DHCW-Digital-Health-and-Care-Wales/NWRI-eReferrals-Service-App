edca2bd51b3b312b1116afc5c712fb803750fb57
Henry <henry.ozoani@fairmoney.io>
2026-01-20 12:57:37 +0000

    add cancel referral validation pipeline and model scaffolding

--- src/NWRI.eReferralsService.API/Models/BundleCancelReferralModel.cs
+++ src/NWRI.eReferralsService.API/Models/BundleCancelReferralModel.cs
@@ -1,13 +1,15 @@
 using Hl7.Fhir.Model;
 using WCCG.eReferralsService.API.Extensions;
+
 namespace WCCG.eReferralsService.API.Models;
 
-public sealed class BundleCancelReferralModel : IBundleModel<BundleCancelReferralModel>
+public sealed class BundleCancelReferralModel
 {
     public required MessageHeader? MessageHeader { get; set; }
     public required ServiceRequest? ServiceRequest { get; set; }
     public required Patient? Patient { get; set; }
     public required List<Organization>? Organizations { get; set; }
+
     public static BundleCancelReferralModel FromBundle(Bundle bundle)
     {
         return new BundleCancelReferralModel
@@ -15,7 +17,7 @@ public sealed class BundleCancelReferralModel : IBundleModel<BundleCancelReferra
             MessageHeader = bundle.ResourceByType<MessageHeader>(),
             ServiceRequest = bundle.ResourceByType<ServiceRequest>(),
             Patient = bundle.ResourceByType<Patient>(),
-            Organizations = bundle.ResourcesByType<Organization>().ToList()
+            Organizations = [.. bundle.ResourcesByType<Organization>()]
         };
     }
 }
