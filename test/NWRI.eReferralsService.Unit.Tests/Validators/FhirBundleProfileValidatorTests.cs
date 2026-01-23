2eea196cba609e9b02f2d913b2bf65fcfae8365d
Patryk Urban <1850832+urbanek32@users.noreply.github.com>
2026-01-23 16:07:53 +0100

    Add tests for FhirBundleProfileValidator initialization and validation scenarios

--- test/NWRI.eReferralsService.Unit.Tests/Validators/FhirBundleProfileValidatorTests.cs
+++ test/NWRI.eReferralsService.Unit.Tests/Validators/FhirBundleProfileValidatorTests.cs
@@ -35,7 +35,65 @@ public class FhirBundleProfileValidatorTests
     }
 
     [Fact]
-    public async Task ValidateShouldThrowWhenEnabledAndNoPackageFilesFoundInDirectory()
+    public async Task ValidateShouldThrowWhenEnabledButNotInitialized()
+    {
+        // Arrange
+        var config = Options.Create(new FhirBundleProfileValidationConfig
+        {
+            Enabled = true
+        });
+
+        var hostEnvironment = new Mock<IHostEnvironment>();
+        hostEnvironment.SetupGet(x => x.ContentRootPath).Returns(Path.GetTempPath());
+
+        var sut = new FhirBundleProfileValidator(config, hostEnvironment.Object, NullLogger<FhirBundleProfileValidator>.Instance);
+
+        // Act
+        var action = async () => await sut.ValidateAsync(new Bundle { Type = Bundle.BundleType.Message });
+
+        // Assert
+        await action.Should().ThrowAsync<InvalidOperationException>()
+            .WithMessage("FHIR Validator must be initialized before use.");
+    }
+
+    [Fact]
+    public void IsInitializedShouldBeFalseBeforeInitialization()
+    {
+        // Arrange
+        var config = Options.Create(new FhirBundleProfileValidationConfig
+        {
+            Enabled = true
+        });
+
+        var hostEnvironment = new Mock<IHostEnvironment>();
+        hostEnvironment.SetupGet(x => x.ContentRootPath).Returns(Path.GetTempPath());
+
+        var sut = new FhirBundleProfileValidator(config, hostEnvironment.Object, NullLogger<FhirBundleProfileValidator>.Instance);
+
+        // Assert
+        sut.IsInitialized.Should().BeFalse();
+    }
+
+    [Fact]
+    public void IsReadyShouldBeFalseBeforeInitialization()
+    {
+        // Arrange
+        var config = Options.Create(new FhirBundleProfileValidationConfig
+        {
+            Enabled = true
+        });
+
+        var hostEnvironment = new Mock<IHostEnvironment>();
+        hostEnvironment.SetupGet(x => x.ContentRootPath).Returns(Path.GetTempPath());
+
+        var sut = new FhirBundleProfileValidator(config, hostEnvironment.Object, NullLogger<FhirBundleProfileValidator>.Instance);
+
+        // Assert
+        sut.IsReady.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task InitializeShouldThrowWhenEnabledAndNoPackageFilesFoundInDirectory()
     {
         // Arrange
         var config = Options.Create(new FhirBundleProfileValidationConfig
@@ -74,7 +132,7 @@ public class FhirBundleProfileValidatorTests
     }
 
     [Fact]
-    public async Task ValidateShouldThrowWhenEnabledAndPackageDirectoryDoesNotExist()
+    public async Task InitializeShouldThrowWhenEnabledAndPackageDirectoryDoesNotExist()
     {
         // Arrange
         var config = Options.Create(new FhirBundleProfileValidationConfig
@@ -106,6 +164,270 @@ public class FhirBundleProfileValidatorTests
             {
                 Directory.Delete(contentRoot, recursive: true);
             }
+        }
+    }
+
+    [Fact]
+    public async Task ValidateShouldThrowWhenValidationTimeoutIsExceeded()
+    {
+        // Arrange
+        var config = Options.Create(new FhirBundleProfileValidationConfig
+        {
+            Enabled = true,
+            ValidationTimeoutSeconds = 0 // Set to 0 to force immediate timeout
+        });
+
+        var contentRoot = SetupTestEnvironmentWithPackages();
+        var hostEnvironment = new Mock<IHostEnvironment>();
+        hostEnvironment.SetupGet(x => x.ContentRootPath).Returns(contentRoot);
+
+        try
+        {
+            var sut = new FhirBundleProfileValidator(
+                config,
+                hostEnvironment.Object,
+                NullLogger<FhirBundleProfileValidator>.Instance);
+
+            await sut.InitializeAsync();
+
+            // Act
+            var action = async () => await sut.ValidateAsync(new Bundle { Type = Bundle.BundleType.Message });
+
+            // Assert
+            await action.Should().ThrowAsync<TimeoutException>()
+                .WithMessage("*timed out*");
+        }
+        finally
+        {
+            CleanupTestEnvironment(contentRoot);
+        }
+    }
+
+    [Fact]
+    public async Task ValidateShouldThrowWhenCancellationTokenIsCancelled()
+    {
+        // Arrange
+        var config = Options.Create(new FhirBundleProfileValidationConfig
+        {
+            Enabled = true
+        });
+
+        var contentRoot = SetupTestEnvironmentWithPackages();
+        var hostEnvironment = new Mock<IHostEnvironment>();
+        hostEnvironment.SetupGet(x => x.ContentRootPath).Returns(contentRoot);
+
+        try
+        {
+            var sut = new FhirBundleProfileValidator(
+                config,
+                hostEnvironment.Object,
+                NullLogger<FhirBundleProfileValidator>.Instance);
+
+            await sut.InitializeAsync();
+
+            var cts = new CancellationTokenSource();
+            await cts.CancelAsync();
+
+            // Act
+            var action = async () => await sut.ValidateAsync(new Bundle { Type = Bundle.BundleType.Message }, cts.Token);
+
+            // Assert
+            await action.Should().ThrowAsync<OperationCanceledException>();
+        }
+        finally
+        {
+            CleanupTestEnvironment(contentRoot);
+        }
+    }
+
+    [Fact]
+    public void DisposeShouldNotThrowException()
+    {
+        // Arrange
+        var config = Options.Create(new FhirBundleProfileValidationConfig
+        {
+            Enabled = false
+        });
+
+        var hostEnvironment = new Mock<IHostEnvironment>();
+        hostEnvironment.SetupGet(x => x.ContentRootPath).Returns(Path.GetTempPath());
+
+        var sut = new FhirBundleProfileValidator(config, hostEnvironment.Object, NullLogger<FhirBundleProfileValidator>.Instance);
+
+        // Act
+        var action = () => sut.Dispose();
+
+        // Assert
+        action.Should().NotThrow();
+    }
+
+    [Fact]
+    public void DisposeShouldNotThrowExceptionWhenCalledMultipleTimes()
+    {
+        // Arrange
+        var config = Options.Create(new FhirBundleProfileValidationConfig
+        {
+            Enabled = false
+        });
+
+        var hostEnvironment = new Mock<IHostEnvironment>();
+        hostEnvironment.SetupGet(x => x.ContentRootPath).Returns(Path.GetTempPath());
+
+        var sut = new FhirBundleProfileValidator(config, hostEnvironment.Object, NullLogger<FhirBundleProfileValidator>.Instance);
+
+        // Act
+        var action = () =>
+        {
+            sut.Dispose();
+            sut.Dispose();
+        };
+
+        // Assert
+        action.Should().NotThrow();
+    }
+
+    [Fact]
+    public async Task DisposeShouldNotThrowExceptionAfterInitialization()
+    {
+        // Arrange
+        var config = Options.Create(new FhirBundleProfileValidationConfig
+        {
+            Enabled = true
+        });
+
+        var contentRoot = SetupTestEnvironmentWithPackages();
+        var hostEnvironment = new Mock<IHostEnvironment>();
+        hostEnvironment.SetupGet(x => x.ContentRootPath).Returns(contentRoot);
+
+        try
+        {
+            var sut = new FhirBundleProfileValidator(
+                config,
+                hostEnvironment.Object,
+                NullLogger<FhirBundleProfileValidator>.Instance);
+
+            await sut.InitializeAsync();
+
+            // Act
+            var action = () => sut.Dispose();
+
+            // Assert
+            action.Should().NotThrow();
+        }
+        finally
+        {
+            CleanupTestEnvironment(contentRoot);
+        }
+    }
+
+    [Fact]
+    public async Task InitializeShouldSetIsInitializedAndIsReadyToTrue()
+    {
+        // Arrange
+        var config = Options.Create(new FhirBundleProfileValidationConfig
+        {
+            Enabled = true
+        });
+
+        var contentRoot = SetupTestEnvironmentWithPackages();
+        var hostEnvironment = new Mock<IHostEnvironment>();
+        hostEnvironment.SetupGet(x => x.ContentRootPath).Returns(contentRoot);
+
+        try
+        {
+            var sut = new FhirBundleProfileValidator(
+                config,
+                hostEnvironment.Object,
+                NullLogger<FhirBundleProfileValidator>.Instance);
+
+            // Act
+            await sut.InitializeAsync();
+
+            // Assert
+            sut.IsInitialized.Should().BeTrue();
+            sut.IsReady.Should().BeTrue();
+        }
+        finally
+        {
+            CleanupTestEnvironment(contentRoot);
+        }
+    }
+
+    [Fact]
+    public async Task InitializeShouldContinueWhenWarmupExampleFileDoesNotExist()
+    {
+        // Arrange
+        var config = Options.Create(new FhirBundleProfileValidationConfig
+        {
+            Enabled = true
+        });
+
+        var contentRoot = SetupTestEnvironmentWithPackages();
+        var hostEnvironment = new Mock<IHostEnvironment>();
+        hostEnvironment.SetupGet(x => x.ContentRootPath).Returns(contentRoot);
+
+        try
+        {
+            var sut = new FhirBundleProfileValidator(
+                config,
+                hostEnvironment.Object,
+                NullLogger<FhirBundleProfileValidator>.Instance);
+
+            // Act
+            await sut.InitializeAsync();
+
+            // Assert
+            sut.IsInitialized.Should().BeTrue();
+            sut.IsReady.Should().BeTrue();
+        }
+        finally
+        {
+            CleanupTestEnvironment(contentRoot);
+        }
+    }
+
+    [Fact]
+    public async Task ValidateShouldReturnSuccessfulOutputWhenDisabledWithoutInitialization()
+    {
+        // Arrange
+        var config = Options.Create(new FhirBundleProfileValidationConfig
+        {
+            Enabled = false
+        });
+
+        var hostEnvironment = new Mock<IHostEnvironment>();
+        hostEnvironment.SetupGet(x => x.ContentRootPath).Returns(Path.GetTempPath());
+
+        var sut = new FhirBundleProfileValidator(config, hostEnvironment.Object, NullLogger<FhirBundleProfileValidator>.Instance);
+
+        // Act
+        var output = await sut.ValidateAsync(new Bundle { Type = Bundle.BundleType.Message });
+
+        // Assert
+        output.IsSuccessful.Should().BeTrue();
+        output.Errors.Should().BeEmpty();
+        sut.IsInitialized.Should().BeFalse();
+        sut.IsReady.Should().BeFalse();
+    }
+
+    private static string SetupTestEnvironmentWithPackages()
+    {
+        var contentRoot = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N"));
+        var packageDir = Path.Combine(contentRoot, "FhirPackages");
+        Directory.CreateDirectory(packageDir);
+
+        // Create a dummy package file
+        var dummyPackageFile = Path.Combine(packageDir, "dummy-package.tgz");
+        File.WriteAllText(dummyPackageFile, "dummy content");
+
+        return contentRoot;
+    }
+
+    private static void CleanupTestEnvironment(string contentRoot)
+    {
+        if (Directory.Exists(contentRoot))
+        {
+            Directory.Delete(contentRoot, recursive: true);
         }
     }
 }
