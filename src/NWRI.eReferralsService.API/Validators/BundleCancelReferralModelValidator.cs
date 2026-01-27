c780c94f18629f55ec1f6c667982488bc0e58ee6
Henry <henry.ozoani@fairmoney.io>
2026-01-27 10:12:09 +0000

    fix extra space

--- src/NWRI.eReferralsService.API/Validators/BundleCancelReferralModelValidator.cs
+++ src/NWRI.eReferralsService.API/Validators/BundleCancelReferralModelValidator.cs
@@ -1,7 +1,5 @@
 using FluentValidation;
-using Hl7.Fhir.Model;
 using NWRI.eReferralsService.API.Models;
-using static NWRI.eReferralsService.API.Constants.ValidationMessages;
 // ReSharper disable NullableWarningSuppressionIsUsed
 
 namespace NWRI.eReferralsService.API.Validators;
