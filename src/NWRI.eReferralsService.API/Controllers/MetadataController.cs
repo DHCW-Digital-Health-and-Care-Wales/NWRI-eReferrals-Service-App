1b00bc4e5eca4e4c4c78302a8c9dedd785c08d8c
Henry <henry.ozoani@fairmoney.io>
2026-02-25 14:45:33 +0000

    moved attributes

--- src/NWRI.eReferralsService.API/Controllers/MetadataController.cs
+++ src/NWRI.eReferralsService.API/Controllers/MetadataController.cs
@@ -3,7 +3,7 @@ using NWRI.eReferralsService.API.Constants;
 using NWRI.eReferralsService.API.Extensions.Logger;
 using NWRI.eReferralsService.API.Middleware;
 using NWRI.eReferralsService.API.Services;
-using NWRI.eReferralsService.API.Swagger;
+using NWRI.eReferralsService.API.Swagger.Attributes;
 
 namespace NWRI.eReferralsService.API.Controllers;
 
