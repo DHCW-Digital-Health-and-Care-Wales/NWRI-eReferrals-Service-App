8e9404eac822c6e7cbe17ec6a2336bd9c764e4de
Dawid Richert <dawidr@kainos.com>
2026-02-26 14:22:20 +0100

    Use Lazy cache to ensure single schema initialization under load

--- src/NWRI.eReferralsService.API/Validators/WpasJsonSchemaValidator.cs
+++ src/NWRI.eReferralsService.API/Validators/WpasJsonSchemaValidator.cs
@@ -9,7 +9,7 @@ public class WpasJsonSchemaValidator
 {
     private const string WpasCreateReferralRequestJsonSchemaPath = "Schemas/WPAS-create-referral-request.schema.json";
 
-    private readonly ConcurrentDictionary<string, JsonSchema> _schemaCache = new();
+    private readonly ConcurrentDictionary<string, Lazy<JsonSchema>> _schemaCache = new();
     private readonly IHostEnvironment _hostEnvironment;
 
     public WpasJsonSchemaValidator(IHostEnvironment hostEnvironment)
@@ -27,7 +27,11 @@ public class WpasJsonSchemaValidator
         var jsonElement = JsonSerializer.SerializeToElement(model);
         var schemaPath = Path.Combine(_hostEnvironment.ContentRootPath, jsonSchemaPath);
 
-        var schema = _schemaCache.GetOrAdd(schemaPath, static path => JsonSchema.FromFile(path));
+        var schema = _schemaCache.GetOrAdd(
+            schemaPath,
+            static path => new Lazy<JsonSchema>(
+                () => JsonSchema.FromFile(path),
+                LazyThreadSafetyMode.ExecutionAndPublication)).Value;
 
         return schema.Evaluate(jsonElement, new EvaluationOptions { OutputFormat = OutputFormat.List });
     }
